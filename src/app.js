8799d8831c69218e62fdbab890405dd660dffcec
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-03-15 14:44:17 -0700

    Added initial database file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+//require('./db/mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
