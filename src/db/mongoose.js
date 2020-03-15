a987b9d7150d4c3d609e777dbc7b45a784580260
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-03-15 15:19:49 -0700

    Final updates

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'URSA Map';
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+    if(error){
+        return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
 });
