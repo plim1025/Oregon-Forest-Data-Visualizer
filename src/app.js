c48bd6d75d48940aa46ffea3f12642f6779d05e5
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-03-23 23:25:39 -0700

    Loaded in firebase to app.js and changed the views directory

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+var firebase = require("firebase-admin");
+var serviceAccount = require("../serviceAccountKey.json");
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://ursa-deb5a.firebaseio.com"
+});
+
+
 const publicDirectory = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../public/views');
 
